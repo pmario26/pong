ae26f220746cbaa08708795620617a87852649d4
pmario26 <pmario26@gmail.com>
2016-06-15 14:01:52 -0300

    Add files via upload

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 import pygame
 from pygame.locals import *
-import random
+import random,math
 
 class Pong(object):
     def __init__(self, screensize):
@@ -19,9 +19,8 @@ class Pong(object):
         self.color = (100,100,255)
         
 
-        vector = [ 1,-1]
-        self.direction = [vector[random.randint(0,1)],vector[random.randint(0,1)]]
-
+        
+        self.direction = [random.randrange(-1,2,2), random.randrange(-1,2,2)]
         self.speedx = 2
         self.speedy = 5
         #CODE TASK: change speed/radius as game progresses to make it harder
@@ -31,7 +30,7 @@ class Pong(object):
         self.hit_edge_right = False
         self.hit_edge_top = False
         self.hit_edge_bot = False
-    def update(self, player_paddle, ai_paddle_0,ai_paddle_1,ai_paddle_2):
+    def update(self, player_paddle, ai_paddle_0,ai_paddle_1,ai_paddle_2, corner_BL, corner_BR, corner_UL, corner_UR ):
 
         self.centerx += self.direction[0]*self.speedx
         self.centery += self.direction[1]*self.speedy
@@ -59,11 +58,66 @@ class Pong(object):
             self.direction[1] *= -1
         if self.rect.colliderect(ai_paddle_2.rect):
             self.direction[1] *= -1        
-
+        #Colisão da bola com os cantos      
+        if pygame.sprite.collide_circle(self, corner_BL):
+            if self.direction[0] > 0:
+                self.direction[0] *= 1.1
+                self.direction[1] *= 1
+            else:
+                self.direction[0] *= -1.1
+                self.direction[1] *= -1                
+        
+        if pygame.sprite.collide_circle(self, corner_BR):
+            if self.direction[0] < 0:
+                self.direction[0] *= 1.1
+                self.direction[1] *= 1.1
+            else:
+                self.direction[0] *= -1.1
+                self.direction[1] *= -1
+    
+        if pygame.sprite.collide_circle(self, corner_UL):
+            if self.direction[0] > 0:
+                self.direction[0] *= 1.1
+                self.direction[1] *= 1
+            else:
+                self.direction[0] *= -1.1
+                self.direction[1] *= -1
+                    
+        if pygame.sprite.collide_circle(self, corner_UR):
+            if self.direction[0] < 0:
+                self.direction[0] *= 1.1
+                self.direction[1] *= 1.1
+            else:
+                self.direction[0] *= -1.1
+                self.direction[1] *= -1
+            
+         
     def render(self, screen):
         pygame.draw.circle(screen, self.color, self.rect.center, self.radius, 0)
         pygame.draw.circle(screen, (0,0,0), self.rect.center, self.radius, 1)
 
+#Os circulos dos cantos
+class Corner(object):
+    def __init__(self, screensize, posx, posy):
+        
+        self.screensize = screensize
+        
+        self.posx = posx
+        self.posy = posy 
+        
+        
+        self.radius = 50
+        
+        self.rect = pygame.Rect(posx - self.radius, posy - self.radius, 2*self.radius, 2*self.radius)        
+        
+        self.color = (0,255,0)
+            
+    def render(self, screen, posx, posy):
+        
+        #pygame.draw.circle(screen, (0,0,0), (posx, posy),75, 1)        
+        pygame.draw.circle(screen, self.color, (posx, posy), self.radius, 0)
+        pygame.draw.circle(screen, (0,0,0), (posx, posy), self.radius, 1) 
+        #pygame.draw.rect(screen, (0,0,0), self.rect, 0)
 
 class AIPaddle_vert(object):
     def __init__(self, screensize):
@@ -83,13 +137,23 @@ class AIPaddle_vert(object):
 
         self.speed = 3
 
-    def update(self, pong):
+    def update(self, pong, corner_UL, corner_BL):
         if pong.rect.top < self.rect.top:
             self.centery -= self.speed
         elif pong.rect.bottom > self.rect.bottom:
             self.centery += self.speed
 
         self.rect.center = (self.centerx, self.centery)
+        #Checar colisão com os cantos
+        if self.rect.colliderect(corner_UL):
+            self.rect.top = corner_UL.radius 
+            self.centery += self.speed
+        elif self.rect.colliderect(corner_BL):
+            self.rect.bottom = self.screensize[1] - corner_BL.radius    
+            self.centery -= self.speed
+        
+        #ajustar a posição
+        self.rect.center = (self.centerx, self.centery)              
 
     def render(self, screen):
         pygame.draw.rect(screen, self.color, self.rect, 0)
@@ -115,19 +179,28 @@ class AIPaddle_hor(object):
 
         self.speed = 3
 
-    def update(self, pong):
+    def update(self, pong, corner_UL, corner_BL):
         if pong.rect.left < self.rect.left:
             self.centerx -= self.speed
         elif pong.rect.right > self.rect.right:
             self.centerx += self.speed
 
         self.rect.center = (self.centerx, self.centery)
-
+        #Checar colisão com os cantos
+        if self.rect.colliderect(corner_UL):
+            self.rect.top = corner_UL.radius 
+            self.centery += self.speed
+        elif self.rect.colliderect(corner_BL):
+            self.rect.bottom = self.screensize[1] - corner_BL.radius    
+            self.centery -= self.speed
+        
+        #ajustar a posição
+        self.rect.center = (self.centerx, self.centery)              
     def render(self, screen):
         pygame.draw.rect(screen, self.color, self.rect, 0)
         pygame.draw.rect(screen, (0,0,0), self.rect, 1)
 
-
+        
 
 class PlayerPaddle(object):
     def __init__(self, screensize):
@@ -148,7 +221,7 @@ class PlayerPaddle(object):
         self.speed = 3
         self.direction = 0
 
-    def update(self):
+    def update(self, corner_UR, corner_BR):
         self.centery += self.direction*self.speed
 
         self.rect.center = (self.centerx, self.centery)
@@ -156,7 +229,13 @@ class PlayerPaddle(object):
             self.rect.top = 0
         if self.rect.bottom > self.screensize[1]-1:
             self.rect.bottom = self.screensize[1]-1
-
+            #Checar colisão com os cantos
+        if self.rect.colliderect(corner_UR):
+            self.rect.top = corner_UR.radius
+            self.direction = 0
+        elif self.rect.colliderect(corner_BR):
+            self.rect.bottom = self.screensize[1] - corner_BR.radius         
+            self.direction = 0
     def render(self, screen):
         pygame.draw.rect(screen, self.color, self.rect, 0)
         pygame.draw.rect(screen, (0,0,0), self.rect, 1)
@@ -186,6 +265,10 @@ def main():
     label_2 = myfont.render(str(score_ai_2),1,(255,255,255))
     label_player = myfont.render(str(score_player),1,(255,255,255))
     running = True
+    cornerBL = Corner(screensize, 0, 480)
+    cornerBR = Corner(screensize, 640, 480)
+    cornerUL = Corner(screensize, 0, 0)
+    cornerUR = Corner(screensize, 640, 0)    
 
     while running:
         #fps limiting/reporting phase
@@ -208,11 +291,11 @@ def main():
                     player_paddle.direction = 0
 
         #object updating phase
-        ai_paddle_0.update(pong)
-        ai_paddle_1.update(pong)
-        ai_paddle_2.update(pong)
-        player_paddle.update()
-        pong.update(player_paddle, ai_paddle_0,ai_paddle_1,ai_paddle_2)
+        ai_paddle_0.update(pong,cornerUL, cornerBL)
+        ai_paddle_1.update(pong,cornerUL, cornerBL)
+        ai_paddle_2.update(pong,cornerUL, cornerBL)
+        player_paddle.update(cornerUR, cornerBR)
+        pong.update(player_paddle, ai_paddle_0,ai_paddle_1,ai_paddle_2,cornerBL, cornerBR, cornerUL, cornerUR)
        
 
         #CODE TASK: make some text on the screen over everything else saying you lost/won, and then exit on keypress
@@ -220,22 +303,22 @@ def main():
         if pong.hit_edge_left:
             print 'You Won'
             score_ai_0 -= 1
-            running = False
+            pong = Pong(screensize)
             label_0 = myfont.render(str(score_ai_0),1,(255,255,255))
         elif pong.hit_edge_top :
             print 'You Won'
             score_ai_1 -= 1
-            running = False
+            pong = Pong(screensize)
             label_1 = myfont.render(str(score_ai_0),1,(255,255,255))
         elif  pong.hit_edge_bot :
             print 'You Won'
             score_ai_2 -= 1
-            running = False  
+            pong = Pong(screensize)  
             label_2 = myfont.render(str(score_ai_0),1,(255,255,255))
         elif pong.hit_edge_right:
             print 'You Lose'
             score_player -= 1
-            running = False
+            pong = Pong(screensize)
             label_player = myfont.render(str(score_player),1,(255,255,255))
 
         #rendering phase
@@ -246,6 +329,10 @@ def main():
         ai_paddle_2.render(screen)
         player_paddle.render(screen)
         pong.render(screen)
+        cornerBL.render(screen, 0, 480)
+        cornerBR.render(screen, 640, 480)
+        cornerUL.render(screen, 0, 0)
+        cornerUR.render(screen, 640, 0)        
         screen.blit(label_player, (585, 415))
         screen.blit(label_0, (0,415))
         screen.blit(label_1, (0,-10))
